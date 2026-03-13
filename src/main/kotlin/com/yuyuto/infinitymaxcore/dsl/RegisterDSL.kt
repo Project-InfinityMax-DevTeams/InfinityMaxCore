92a3975f8bbdb9f4c51da71d0bfb8e871f98f386
yuyuto000 <yuyuto0514@gmail.com>
2026-03-13 21:48:16 +0900

    Blockの登録DSL完成

--- src/main/kotlin/com/yuyuto/infinitymaxcore/dsl/RegisterDSL.kt
+++ src/main/kotlin/com/yuyuto/infinitymaxcore/dsl/RegisterDSL.kt
@@ -3,6 +3,9 @@ package com.yuyuto.infinitymaxcore.dsl
 import com.yuyuto.infinitymaxcore.logic.LogicPhase
 import com.yuyuto.infinitymaxcore.registry.BlockValueStorage
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.material.MapColor
 
 class BlockDSLBuilder(private val storage: BlockValueStorage){
 
@@ -16,6 +19,34 @@ class BlockDSLBuilder(private val storage: BlockValueStorage){
 
     fun light(value: Int){
         storage.lightLevel = value
+    }
+
+    fun friction(value: Float){
+        storage.friction = value
+    }
+
+    fun blockIssuance(issuance: BlockBehaviour.StatePredicate){
+        storage.emissiveRenderer = issuance
+    }
+
+    fun mapColor(color: MapColor){
+        storage.mapColor = color
+    }
+
+    fun blockSoundType(type: SoundType){
+        storage.soundType = type
+    }
+
+    fun breakingToolRequire(required: Boolean){
+        storage.isRequireToolForDrop = required
+    }
+
+    fun doLavaFire(lavaFire: Boolean){
+        storage.isIgnitedByLava = lavaFire
+    }
+
+    fun hasBlockItem(created: Boolean){
+        storage.isHasBlockItem = created
     }
 
     fun texture(id: String){
