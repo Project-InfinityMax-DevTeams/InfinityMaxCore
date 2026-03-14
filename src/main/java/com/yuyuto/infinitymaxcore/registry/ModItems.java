89bebc7aef4ce38f2e11d1e04e4eb63237ec9909
yuyuto000 <yuyuto0514@gmail.com>
2026-03-14 19:32:39 +0900

    アイテム登録についての引数の書き方が絶望的にわかんなかったのでここまで。

---
+++ src/main/java/com/yuyuto/infinitymaxcore/registry/ModItems.java
@@ -0,0 +1,26 @@
+package com.yuyuto.infinitymaxcore.registry;
+
+import com.yuyuto.infinitymaxcore.item.ItemValueStorage;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,"infinitymaxcore");
+    public static void register(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    public static RegistryObject<Item> registryItem(@NotNull ItemValueStorage storage){
+
+        RegistryObject<Item> object = ModItems.ITEMS.register(
+                storage.getItemId(),()
+                -> ItemFactory.create(storage)
+        );
+
+        ITEM_MAP.put(storage.getItemId(),object);
+        return object;
+    }
+}
